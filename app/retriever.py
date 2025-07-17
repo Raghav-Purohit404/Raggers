02cbfa16651f7d9507d1590a1bce739702a9c8d8
Raghav Purohit <raghavpurohit71@gmail.com>
2025-07-17 11:20:10 +0530

    deleted useless lines

--- app/retriever.py
+++ app/retriever.py
@@ -1,13 +1,2 @@
 # Retriever module
 
-git clone https://github.com/Raghav-Purohit404/Raggers.git
-cd raggers
-"C:\Program Files\Git\bin\git.exe"
-git add .
-git commit -m "Edition 1.0"
-git config --global user.name "Raghav-Purohit404"
-git config --global user.email "raghavpurohit71@gmail.com"
-git add .
-git commit -m "Edition 1.0"
-git push origin main
-git push -u Raggers main
